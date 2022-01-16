68336752d386c9e3c5af81290ffe39b3c2c83c2f
Tausif4171 <tausifkhan4171@gmail.com>
2022-01-16 12:46:34 +0530

    var, let, const keyword

---
+++ index.js
@@ -0,0 +1,32 @@
+
+// ES6 Tutorial: Learn Modern JavaScript 🙂
+
+
+// var -> variable declared with the VAR keyword are scoped to the function
+
+// function Test(){
+//     for(var i=0; i<3; i++){
+//         console.log(i);
+//     }
+//     console.log(i);
+// }
+// Test();
+
+
+// let -> variable declared with the LET keyword are scoped to the block in which they are defined
+
+// function Test1(){
+//     for(let i=0; i<3; i++){
+//         console.log(i);
+//     }
+//     console.log(i);
+// }
+// Test1();
+
+
+//const -> we use const keyword to define constant with similar to let keyword like variable declared with the const keyword are scoped to the block
+//         in which they are defined.
+
+// const j = 2;
+// j = 3;
+// console.log(j);
